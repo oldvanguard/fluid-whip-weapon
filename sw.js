5b7a0b63da5d88dde9e9d3fe9830d34027d825fa
oldvanguard <mynameiscarew@gmail.com>
2026-08-19 18:26:32 -0400

    Cache trainable WHIP engine offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fww-v5';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./fluid-data.js','./whip-data.js','./movement-visuals.js','./visual-engine.js','./training-engine.js','./visual-integration.js','./stage-integration.js','./manifest.webmanifest'];
+const CACHE='fww-v6';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./fluid-data.js','./whip-data.js','./movement-visuals.js','./visual-engine.js','./training-engine.js','./visual-integration.js','./stage-integration.js','./whip-training.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
