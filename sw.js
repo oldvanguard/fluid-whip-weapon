abf942c29628be388b27078e3045ef3123a5b02a
oldvanguard <mynameiscarew@gmail.com>
2026-08-19 18:15:57 -0400

    Cache curriculum and visual data offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fww-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='fww-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./fluid-data.js','./movement-visuals.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
